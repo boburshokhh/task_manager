d7c974eff064b7aeee4207c1ae199ee0784e6f93
Бобур <boburboy200216b@mail.ru>
2024-11-17 12:13:21 +0500

    Part2

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,28 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const bodyParser = require("body-parser");
+const { sequelize } = require("./models"); // Импортируйте настроенный Sequelize
+const cors = require("cors");
+const morgan = require("morgan");
+const config = require("./config/config");
 
+const app = express();
 
+// Middlewares
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
 
-const app = express()
-app.use(morgan("combine"))
-app.use(bodyParser.json())
-app.use(cors())
+// Routes
+require("./routes")(app);
 
-
-app.post("/register",(req,res)=>{
-    res.send({
-        message:"Your user was registered!"
+// Запуск сервера после синхронизации с базой данных
+sequelize
+    .sync()
+    .then(() => {
+        app.listen(config.port, () => {
+            console.log(`Server started on port ${config.port}`);
+        });
     })
-})
-
-app.listen(process.env.PORT || 8081)
+    .catch((error) => {
+        console.error("Unable to start server:", error);
+    });
